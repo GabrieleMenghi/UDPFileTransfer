e872df99e44241f1ed9d42914b4a11fa0fde7c00
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-13 14:48:40 +0200

    Add thread managing different commands in server.py

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,7 @@
 import socket as sk
+import threading, os
+
+size = 4096
 
 #Socket creation
 socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
@@ -6,3 +9,22 @@ socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 #Socket binding
 server_address = ('localhost', 10000)
 socket.bind(server_address)
+
+lock = threading.Lock()
+ 
+welcome_message = '\r\nUDP file transfer\r\n\r\nOpzioni disponibili\r\n\r\n"list" restituisce la Lista dei file disponibili\r\n"get <filename>" consente di scaricare un file dal server\r\n"put <filepath> <filename>" consente di caricare un file sul server, specificandone il percorso\r\n'
+
+class daemon(threading.Thread):
+    def __init__(self, a):
+        threading.Thread.__init__(self)
+        self.socket = a[0]
+        self.address = a[1]
+    def run(self):
+        #Show welcome message
+        self.socket.sendto(welcome_message.encode(), server_address)
+        #TODO manage different commands
+        #while(True):
+            
+     
+while True:
+    daemon(socket.accept()).start()
