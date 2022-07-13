701deaf2cd0c8c7c742f2473a530d82d07b56b45
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-13 13:01:37 +0200

    Create basic UDP client

--- Client/client.py
+++ Client/client.py
@@ -3,6 +3,6 @@ import socket as sk
 #Socket creation
 socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 
-#Socket binding
 server_address = ('localhost', 10000)
-socket.bind(server_address)
+
+socket.close()
