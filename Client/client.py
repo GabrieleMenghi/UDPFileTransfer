445e291c14ab89491494762837397a3c0ad2ccbe
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-13 16:34:53 +0200

    Add basic functionalities in server.py

--- Client/client.py
+++ Client/client.py
@@ -1,8 +1,19 @@
 import socket as sk
 
+size = 4096
+
 #Socket creation
 socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 
 server_address = ('localhost', 10000)
 
-socket.close()
+message = 'Messaggio di prova'
+
+try:
+    socket.sendto(input().encode(), server_address)
+    data, server = socket.recvfrom(size)
+    print(data.decode())
+except Exception as e:
+    print(e)
+finally:
+    socket.close()
--- Server/server.py
+++ Server/server.py
@@ -8,12 +8,14 @@ socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 
 #Socket binding
 server_address = ('localhost', 10000)
+print ('\n\rstarting on %s port %s' % server_address)
 socket.bind(server_address)
 
 lock = threading.Lock()
  
 welcome_message = '\r\nUDP file transfer\r\n\r\nOpzioni disponibili\r\n\r\n"list" restituisce la Lista dei file disponibili\r\n"get <filename>" consente di scaricare un file dal server\r\n"put <filepath> <filename>" consente di caricare un file sul server, specificandone il percorso\r\n'
 
+"""
 class daemon(threading.Thread):
     def __init__(self, a):
         threading.Thread.__init__(self)
@@ -21,10 +23,24 @@ class daemon(threading.Thread):
         self.address = a[1]
     def run(self):
         #Show welcome message
-        self.socket.sendto(welcome_message.encode(), server_address)
+        sent = self.socket.sendto(welcome_message.encode(), server_address[0])
+        print(sent)
         #TODO manage different commands
         #while(True):
+        self.socket.close()
             
      
 while True:
     daemon(socket.accept()).start()
+
+"""
+while True:
+    data, address = socket.recvfrom(size)
+    #socket.sendto(welcome_message.encode(), address)
+    if data.decode().split()[0] == 'list':
+        socket.sendto('Listing'.encode(), address)
+    elif data.decode().split()[0] == 'get':
+        socket.sendto('Getting'.encode(), address)
+    elif data.decode().split()[0] == 'put':
+        socket.sendto('Putting'.encode(), address)
+    print(data.decode())
